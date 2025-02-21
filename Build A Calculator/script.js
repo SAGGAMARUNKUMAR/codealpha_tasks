c3044065de1edfb696e525a195e21c8186dc4dba
SAGGAM ARUN KUMAR <168418314+SAGGAMARUNKUMAR@users.noreply.github.com>
2025-02-21 22:05:10 +0530

    Add files via upload

---
+++ Build A Calculator/script.js
@@ -0,0 +1,63 @@
+let previousOperandTextElement = document.getElementById('previous-operand');
+let currentOperandTextElement = document.getElementById('current-operand');
+
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function appendNumber(number) {
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') calculate();
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function calculate() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    currentOperandTextElement.innerText = currentOperand;
+    if (operation != null) {
+        previousOperandTextElement.innerText = `${previousOperand} ${operation}`;
+    } else {
+        previousOperandTextElement.innerText = '';
+    }
+}
